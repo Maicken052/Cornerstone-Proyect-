81a5145d90c8b68e752d50ac3ea874d2068c6a51
Maicken <robert.fonseca@urosario.edu.co>
2023-03-20 22:55:07 -0500

    v 0.2
    Pantalla principal creada

--- app_corner/lib/main.dart
+++ app_corner/lib/main.dart
@@ -1,115 +1,177 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; //Importar widgets de flutter
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(App()); //Funcion void que ejecuta el primer widget de la aplicación
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget { 
+const App({ Key? key }) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){ //Crear la app con estilo 'Material'
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "Pet Feed",
+      home: Inicio(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Inicio extends StatefulWidget {  
+  const Inicio({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Inicio> createState() => _InicioState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _InicioState extends State<Inicio> { //Cuerpo de la app
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Pet Feed"), //Titulo de la barra principal
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+      body: Column( //Columna principal
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+
+          Container(  //Logo de la esquina
+            padding: EdgeInsets.all(5.0),
+            width: MediaQuery.of(context).size.width,
+            height: 50.0,
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Image.asset('images/logo.png'),
+            ),    
+          ),
+
+          Row(  //Row para colocar el rectangulo azul
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+
+              Expanded(
+                child: Container(  //Rectangulo azul
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Color.fromARGB(255, 221, 229, 235)
+                  ),
+                  
+                  child: Column(  //Columna con la foto y el texto 
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      
+                      SizedBox(  //!Separador
+                        height: 10
+                      ),
+
+                      ClipOval(  //Foto de bienvenida
+                        child: Image.asset(
+                          "images/dog.jpg",  //TODO: Hacer que esto se pueda cambiar a gusto
+                          width: 170,
+                          height: 170,
+                          ),
+                      ),
+
+                      SizedBox(  //!Separador
+                        height: 20,
+                      ),
+
+                      Text(  //Texto de bienvenida
+                        "Hello, Sebastian!",  //TODO: Hacer que esto cambie según el deuño
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontFamily: 'JosefinSans',
+                          color: Color.fromARGB(255, 48, 42, 42),
+                        )
+                      ),
+                    ]
+                  )
+                ),
+              ),
+            ]
+          ),
+        
+        SizedBox(  //!Separador
+          height: 20,
+        ),
+
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            const Text(
-              'contador de pulsasiones',
+            
+            Container(
+              width: 170,
+              child: Text(
+                "Your pets:",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'JosefinSans',
+                  color: Color.fromARGB(255, 42, 55, 243),
+                )
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+
+            ElevatedButton(
+              child: Image.asset(
+                'images/your_pets.png',
+                width: 140,
+                height: 160,
+              ),
+              onPressed: null,  //TODO: Agregar lógica del botón
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20), 
+                ),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+        SizedBox(  //!Separador
+          height: 30,
+        ),
+
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            
+            Container(
+              width: 170,
+              child: Text(
+                "Add pet:",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'JosefinSans',
+                  color: Color.fromARGB(255, 42, 55, 243),
+                )
+              ),
+            ),
+
+            ElevatedButton(
+              child: Image.asset(
+                'images/add_pet.png',
+                width: 140,
+                height: 160,
+              ),
+              onPressed: null,  //TODO: Agregar lógica del botón
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20), 
+                ),
+              ),
+            ),
+          ],
+        ),
+
+        ]
+      )
     );
   }
 }
+
+
+
